5f1cbdfa8d2ee1fc99ced2578524ea1128338b79
Aliaksei Budavei <0x000c70@gmail.com>
2024-09-08 23:28:02 +0300

    Add the jdk.jfr and java.logging optional dependencies

--- module_info/src/org.module.info.demo/classes/module-info.java
+++ module_info/src/org.module.info.demo/classes/module-info.java
@@ -1,6 +1,8 @@
 /** Defines demo related support. */
 module org.module.info.demo
 {
+	requires static jdk.jfr;
+	requires static java.logging;
 	requires transitive org.module.info.tester;
 
 	exports org.demo;
